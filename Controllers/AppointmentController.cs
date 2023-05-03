364f499649ec0a087adbfdb888e582229021a9dc
rudzzz <rudsolza13@gmail.com>
2023-05-03 19:31:30 -0300

    controllers folder

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,25 @@
+﻿using AppointmentWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentWebApp.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public static List<AppointmentModel> appointments = new List<AppointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details(AppointmentModel appointment)
+        {
+            appointments.Add(appointment);
+            return View("Details", appointment);
+        }
+    }
+}
